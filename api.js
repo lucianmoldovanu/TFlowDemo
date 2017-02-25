a272d87d1703203eec0b73c1b35e5c6ccf449076
lucianmoldovanu <lucianmoldovanu@users.noreply.github.com>
2017-02-25 02:12:57 +0100

    Update api.js

--- api.js
+++ api.js
@@ -6,14 +6,13 @@ const app = express();
 
 app.post('/classifyImage', function(req, res) {
     var form = new mp.Form();
-    //console.log('received...');
     form.parse(req, function(err, fields, files) {
         var imgPath = files['picture'][0].path;
-        console.log(imgPath);
-
+        
         exec('python /app/tf/icecat_classify.py --model_file /app/tf/output_graph.pb --label_file /app/tf/output_labels.txt --softmax_layer final_result --image_file ' + imgPath,
             function(error, stdout, stderr) {
-                console.log([error,stdout,stderr].join(" | "));
+                //console.log([error,stdout,stderr].join(" | "));
+                res.send(stdout);
             }
         );
     });
