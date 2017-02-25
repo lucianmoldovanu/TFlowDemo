b1f141e1edb840c7e3f115cae68861f13bbe1cfc
lucianmoldovanu <lucianmoldovanu@users.noreply.github.com>
2017-02-25 02:44:23 +0100

    Update and rename api.js to app/server.js

--- app/server.js
+++ app/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
-var exec = require('child_process').exec
-var fs = require('fs');
-var mp = require('multiparty')
+const exec = require('child_process').exec
+const fs = require('fs');
+const mp = require('multiparty')
+
 const app = express();
 
+//webapp
+app.use('/', express.static(path.join(__dirname,'webapp')));
+
+//REST endpoint
+/*
+ * Receive an image, pass it to classifier (python) and return response (class probabilities)
+ * Prerequisite: input is fed via HTTP POST as FORM, with a single picture (jpg) in the field 'picture'
+*/
 app.post('/classifyImage', function(req, res) {
     var form = new mp.Form();
     form.parse(req, function(err, fields, files) {
-        var imgPath = files['picture'][0].path;
+        //var imgPath = files['picture'][0].path;
+        var imgPath = files['myFileUpload'][0]['path'];
         
         exec('python /app/tf/icecat_classify.py --model_file /app/tf/output_graph.pb --label_file /app/tf/output_labels.txt --softmax_layer final_result --image_file ' + imgPath,
             function(error, stdout, stderr) {
