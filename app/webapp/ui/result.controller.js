b58e0aa0381444523cc28104c91127e9ba154fed
lucianmoldovanu <lucianmoldovanu@users.noreply.github.com>
2017-02-25 04:14:48 +0100

    Update result.controller.js

--- app/webapp/ui/result.controller.js
+++ app/webapp/ui/result.controller.js
@@ -1,7 +1,7 @@
 sap.ui.controller("hack.ui.result", {
 	createDataModel: function(aData) {
 		var oData = aData.map(function(rec) {return {row: rec}});
-		var oModel = new sap.ui.model.json.JSONModel(oData);
+		var oModel = new sap.ui.model.json.JSONModel({data: oData});
 		this.getView().byId("idTable").setModel(oModel);
 		oModel.refresh();
 	}
