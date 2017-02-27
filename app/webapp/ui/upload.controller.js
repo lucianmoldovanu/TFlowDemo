b3e0ae02f28f1f3b3130ffa3eef8a10b379fe34d
lucianmoldovanu <lucianmoldovanu@users.noreply.github.com>
2017-02-27 10:45:05 +0100

    Update upload.controller.js

--- app/webapp/ui/upload.controller.js
+++ app/webapp/ui/upload.controller.js
@@ -15,7 +15,7 @@ sap.ui.controller("hack.ui.upload", {
 	handleUploadComplete: function(oEvent) {
 		sap.ui.getCore().byId('idApp').to('Result');
 		//sap.ui.controller('hack.ui.upload')
-		this.createDataModel(oEvent.getParameter('response').split('\n').filter(function(rec) {return rec != ""});
+		this.createDataModel(oEvent.getParameter('response').split('\n').filter((rec) => rec != ""));
 	},
 	
 	createDataModel: function(aData) {
@@ -41,45 +41,41 @@ sap.ui.controller("hack.ui.upload", {
 
 		//Construct the word cloud's SVG element
 		var svg = d3.select(selector).append("svg")
-		.attr("width", 500)
-		.attr("height", 500)
-		.append("g")
-		.attr("transform", "translate(250,250)");
-
+			.attr("width", 500)
+			.attr("height", 500)
+			.append("g")
+			.attr("transform", "translate(250,250)");
 
 		//Draw the word cloud
 		function draw(words) {
-		var cloud = svg.selectAll("g text")
-		.data(words, function(d) { return d.text; })
+			var cloud = svg.selectAll("g text")
+				.data(words, function(d) { return d.text; })
 
-		//Entering words
-		cloud.enter()
-		.append("text")
-		.style("font-family", "Impact")
-		.style("fill", function(d, i) { return fill(i); })
-		.attr("text-anchor", "middle")
-		.attr('font-size', 1)
-		.text(function(d) { return d.text; });
+			//Entering words
+			cloud.enter()
+				.append("text")
+				.style("font-family", "Impact")
+				.style("fill", function(d, i) { return fill(i); })
+				.attr("text-anchor", "middle")
+				.attr('font-size', 1)
+				.text(function(d) { return d.text; });
 
-		//Entering and existing words
-		cloud
-		.transition()
-		.duration(600)
-		.style("font-size", function(d) { return d.size + "px"; })
-		.attr("transform", function(d) {
-		return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-		})
-		.style("fill-opacity", 1);
+			//Entering and existing words
+			cloud
+				.transition().duration(600)
+				.style("font-size", function(d) { return d.size + "px"; })
+				.attr("transform", function(d) {
+				return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+				})
+				.style("fill-opacity", 1);
 
-		//Exiting words
-		cloud.exit()
-		.transition()
-		.duration(200)
-		.style('fill-opacity', 1e-6)
-		.attr('font-size', 1)
-		.remove();
+			//Exiting words
+			cloud.exit()
+				.transition().duration(200)
+				.style('fill-opacity', 1e-6)
+				.attr('font-size', 1)
+				.remove();
 		}
-
 
 		//Use the module pattern to encapsulate the visualisation code. We'll
 		// expose only the parts that need to be public.
@@ -89,16 +85,16 @@ sap.ui.controller("hack.ui.upload", {
 		// asycnhronously call draw when the layout has been computed.
 		//The outside world will need to call this function, so make it part
 		// of the wordCloud return value.
-		update: function(words) {
-		d3.layout.cloud().size([500, 500])
-		.words(words)
-		.padding(5)
-		.rotate(function() { return ~~(Math.random() * 2) * 90; })
-		.font("Impact")
-		.fontSize(function(d) { return d.size; })
-		.on("end", draw)
-		.start();
-		}
+			update: function(words) {
+				d3.layout.cloud().size([500, 500])
+				.words(words)
+				.padding(5)
+				.rotate(function() { return ~~(Math.random() * 2) * 90; })
+				.font("Impact")
+				.fontSize(function(d) { return d.size; })
+				.on("end", draw)
+				.start();
+			}
 		}
 
 	},
