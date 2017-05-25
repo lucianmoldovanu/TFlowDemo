264d068dafec0807c613843e225b49e269d9c9d2
lucianmoldovanu <lucianmoldovanu@users.noreply.github.com>
2017-05-25 20:44:57 +0300

    Create dummyServer.js

---
+++ app/dummyServer.js
@@ -0,0 +1,18 @@
+const express = require('express');
+//const exec = require('child_process').exec;
+const fs = require('fs');
+const mp = require('multiparty');
+const path = require('path');
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.status(200).send('response: dummyServer running on Docker is alive!');
+});
+
+const port = 3000; //process.env.PORT || 8080;
+//port set to 3000 because, when deploying container in CloudFoundry, lowest port is automatically used
+//beyond this port for the app, the container also exposes 6006 and 8888 (tensorboard, tensorflow admin)
+
+app.listen(port);
+console.log('Running dummyServer on http://localhost:' + port + '...');
