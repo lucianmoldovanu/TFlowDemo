fc1a3cdef2a4359da8cefe9217eadfb41297b7c1
lucianmoldovanu <lucianmoldovanu@users.noreply.github.com>
2017-05-25 16:41:50 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -29,7 +29,7 @@ app.post('/classifyImage', function(req, res) {
     });
 });
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8080;
 
 app.listen(port);
 console.log('Running on http://localhost:port... REST endpoint: /classifyImage ; webapp: /');
